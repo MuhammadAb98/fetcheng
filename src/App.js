75fdcb6b465b69b4d7e2a2f175bd48eda7dd2c87
Muhammad Abdullah <56094500+MuhammadAb98@users.noreply.github.com>
2020-12-10 23:53:12 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+
+import React,{ useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+ let data={title:"Waitinfg for Data"};
+
+  const [todo,setTodo]=useState(data);
+  const [isData,setData]=useState(false);
+  
+  const[isFetching,setFetching]=useState(true);
+
+  useEffect(
+
+    ()=>{
+       
+      async function fetchData(){
+
+      
+      const response=await fetch('https://jsonplaceholder.typicode.com/todos/1');
+      console.log("Response ",response);
+      
+      let data2=await response.json(); 
+      setTodo(data2);
+      setFetching(false);
+      console.log("Data value ",todo)
+    }
+
+    fetchData();
+    },[isData]
+    
+  );
+  
+   if(isFetching){
+     return <div>Data Loading</div>
+   }
+  
+  return (
+    <div>
+      Hello World
+  <span>Title:{todo.title}</span> 
+    </div> 
+  );
+}
+
+export default App;
---
+++ src/PostList.js
@@ -0,0 +1,14 @@
+
+import React,{ useEffect, useState } from 'react';
+import './App.css';
+
+function PostList() {
+ return (
+  <div>
+      <h1>Hello 2</h1>
+  </div>
+
+ );
+}
+
+export default PostList;
